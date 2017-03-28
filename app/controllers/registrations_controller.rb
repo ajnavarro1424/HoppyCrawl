e5dc8ff50e1d5f465b650fce45ec8523bc007c5c
Alex Navarro <ajnavarro1424@gmail.com>
2017-03-28 15:52:50 -0700

    Age > 21 validation added

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,9 @@ class RegistrationsController < Devise::RegistrationsController
   private
   def sign_up_params
     params.require(:user).permit(:name, :dob, :email, :password, :password_confirmation)
-		
+
+
+
   end
 
   def account_update_params
